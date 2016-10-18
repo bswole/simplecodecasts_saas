a9cb587c8b2e285047b3ccc92e773622aea7b144
B. Swole <smk1986@yahoo.com>
2016-10-18 14:52:50 +0000

    Removed quotes from port number 587.

--- config/environment.rb
+++ config/environment.rb
@@ -8,10 +8,10 @@ Rails.application.initialize!
 # heroku mailer settings
 ActionMailer::Base.smtp_settings = {
     :address => 'smtp.sendgrid.net',
-    :port => '587',
+    :port => 587,
     :authentication => :plain,
     :user_name => ENV['SENDGRID_USERNAME'],
-    :password => ENV['SENDGRIG_PASSWORD'],
+    :password => ENV['SENDGRID_PASSWORD'],
     :domain => 'heroku.com',
     :enable_startstls_auto => true
 }
