aa3083daaac25c04d4378d186b08f2e205b78266
B. Swole <smk1986@yahoo.com>
2016-10-18 13:45:50 +0000

    Added mailer and view

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -6,9 +6,17 @@ class ContactsController < ApplicationController
     def create
         @contact = Contact.new(contact_params)
         if @contact.save
+            #generate and send email update
+            name = params[:contact][:name]
+            email = params[:contact][:email]
+            body = params[:contact][:comments]
+            ContactMailer.contact_email(name, email, body).deliver
+            
+            #inform user of success
             flash[:success] = 'Message sent.'
             redirect_to new_contact_path
         else
+            #inform user of error
             flash[:danger] = 'Error occured, message has not been sent.'
             redirect_to new_contact_path
         end
---
+++ app/mailers/contact_mailer.rb
@@ -0,0 +1,9 @@
+class ContactMailer < ActionMailer::Base
+    default to: 'smk1986@yahoo.com'
+    
+    def contact_email(name, email, body)
+        @name = name
+        @email = email
+        @body = body
+        mail(from: email, subject: 'Contact Form Message')
+    end
