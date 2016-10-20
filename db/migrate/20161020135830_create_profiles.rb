830e906e47961909201045642d50bcbc3cf0e5ed
B. Swole <smk1986@yahoo.com>
2016-10-20 16:06:58 +0000

    Added profiles form and create action.

--- db/migrate/20161020135830_create_profiles.rb
+++ db/migrate/20161020135830_create_profiles.rb
@@ -3,6 +3,7 @@ class CreateProfiles < ActiveRecord::Migration
     create_table :profiles do |t|
       t.integer :user_id #ties profile to user
       t.string :first_name
+      t.string :last_name
       t.string :job_title
       t.string :phone_number
       t.string :contact_email
